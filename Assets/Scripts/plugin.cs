e63b916983add0193efd6af7030d3248fca6e470
clucle <wjdenwls123@gmail.com>
2018-01-12 14:59:20 +0900

    feat: googleLogin works

---
+++ Assets/Scripts/plugin.cs
@@ -0,0 +1,46 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class plugin : MonoBehaviour
+{
+
+	public Text UId;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		PlayGamesPlatform.DebugLogEnabled = true;
+		PlayGamesPlatform.Activate();
+		LogIn();
+	}
+
+	public void LogIn()
+	{
+		Social.localUser.Authenticate((bool success) =>
+		{
+			if (success)
+			{
+				Debug.Log("login success");
+				Debug.Log(Social.localUser.id);
+				Debug.Log(Social.localUser.userName);
+				UId.text = Social.localUser.id;
+			}
+			else
+			{
+				Debug.Log("login fail");
+			}
+
+		});
+	}
+
+	public void LogOut()
+	{
+		Debug.Log("LogOut");
+		((PlayGamesPlatform)Social.Active).SignOut();
+	}
+}
